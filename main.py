90ec7dabd0e3fbe3dc74216e420f476b0e0870b1
Akash Gupta <ag308669@gmail.com>
2024-06-23 22:23:00 +0530

    Final commit

--- main.py
+++ main.py
@@ -12,13 +12,17 @@ model = joblib.load("models/logistic_regression_model.joblib")
 # Load your scaler (saved using joblib)
 scaler = joblib.load('models/scaler.pkl')
 
+
 @app.get("/")
 def home():
     return jsonify({
         "message": "Server running..."
     })
 
-feature_names = ["age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", "thalach", "exang", "oldpeak", "slope", "ca", "thal"]
+
+feature_names = ["age", "sex", "cp", "trestbps", "chol", "fbs",
+                 "restecg", "thalach", "exang", "oldpeak", "slope", "ca", "thal"]
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -31,17 +35,22 @@ def predict():
 
     # Create a list of all values
     values_list = [data[key] for key in feature_names]
-    
+
     # Create a DataFrame with the input data
     input_df = pd.DataFrame([values_list], columns=feature_names)
+
+    print(input_df)
 
     # Scale input data using the loaded scaler
     scaled_data = scaler.transform(input_df)
 
+    print(scaled_data)
+
     # Predict using the model
     prediction = model.predict(scaled_data)
 
-    prediction_message = "Healthy" if prediction[0] == 0 else "Consult your doctor"
+    prediction_message = "Your heart seems to be in a healthy condition" if prediction[
+        0] == 0 else "Your heart seems to be unhealthy, please consult your doctor"
 
     return jsonify({
         "message": "Prediction successful",
